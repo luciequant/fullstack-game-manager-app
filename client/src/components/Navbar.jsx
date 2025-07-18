1ca9c39ca22522ef3f7c6b39f54130a0ff7eebe9
luciequant <156855912+luciequant@users.noreply.github.com>
2025-07-18 15:20:37 +0200

    Set up Routes

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-title"> MyGameSite</div>
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/create">Add Game</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <App />
+      </Router>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
